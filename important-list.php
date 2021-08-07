33bc72d3a468d20e03007efcbfc9294d302cee70
md mahbub <aidmahbub@gmail.com>
2021-08-07 19:07:34 +0600

    insert data code added

--- important-list.php
+++ important-list.php
@@ -1,6 +1,20 @@
 <?php
-// secure single data show php
+// securly insert data
 
+$job_checker = "INSERT INTO job_post(client_id,client_name,category, job_title, job_details, proof_details, work_limit, worker_earn, total_amount,prooft_need,status) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+$stmt = mysqli_stmt_init($heart);
+if(!mysqli_stmt_prepare($stmt, $job_checker)){
+echo "Error: " . $job_checker . "<br>" . $heart->error;
+}
+else {
+mysqli_stmt_bind_param($stmt, "sssssssssss", $client_id,$client_name,$parentCategory,$job_title,$job_details,$job_proof,$worker_need,$per_work_earn,$client_budget,$prooft_img,$status);
+if (mysqli_stmt_execute($stmt)) {
+echo "<br/><span class='alert alert-success inline-block'><i class='fas fa-check logo-icon'></i> Post Success</span>";
+}
+
+//============================= end ===============
+
+// secure single data show php
 $data_query = "SELECT * FROM $tabel WHERE $whatby=?";
 $stmt = mysqli_stmt_init($heart);
 if(!mysqli_stmt_prepare($stmt, $data_query)){
@@ -12,7 +26,6 @@ mysqli_stmt_execute($stmt);
 $data_result = mysqli_stmt_get_result($stmt);
 $row = mysqli_fetch_assoc($data_result);
 }
-
 // secure all data with count show php
 $data_query = "SELECT *, COUNT(*) AS totaldata FROM users";
 $stmt = mysqli_stmt_init($heart);
@@ -27,28 +40,23 @@ while($row = mysqli_fetch_assoc($data_result)){
 echo $row['totaldata']."<br>";
 echo $row['full_name']."<br>";
 }
-
+//============================= end ===============
 
 // secure update data base
 // ===================== query ===============
 $users = "UPDATE users SET full_name=?,email=?,mobile=?,address=?,about=? WHERE id=?";
-
 $stmt = mysqli_stmt_init($heart);
-
 if(!mysqli_stmt_prepare($stmt, $users )){
 echo "Error: " . $users  . "<br>" . $heart->error;
 }
 else {
-
 mysqli_stmt_bind_param($stmt, "ssssss", $full_name,$email,$mobile,$address,$user_about,$user_id);
 mysqli_stmt_execute($stmt);
 $lesupdate = mysqli_stmt_execute($stmt);
 if($lesupdate){
-    echo '<span class="alert alert-success block">Profile Update Success</span>';
-}    
-}// no sql error 
-
-
+echo '<span class="alert alert-success block">Profile Update Success</span>';
+}
+}// no sql error
 // secure pagination
 // ===================== query ===============
 $limit = 10;
@@ -67,24 +75,24 @@ if (mysqli_num_rows($result1) > 0) {
 $total_records = mysqli_num_rows($result1);
 $total_pages = ceil($total_records / $limit);
 echo '<ul class="pagination">';
-	if($num_page > 1){
-	echo '<li class="page-item">
-		<a class="page-link" href="donar-list.php?page='.($num_page - 1).'">Previous</a>
-	</li>';
-	}
-	for($i = 1; $i <= $total_pages; $i++){
-	if($i == $num_page){
-	$active = "active";
-	} else {
-	$active = "";
-	}
-	echo '<li class="page-item '.$active.'"><a class="page-link" href="donar-list.php?page='.$i.'">'.$i.'</a></li>';
-	}
-	if($total_pages > $num_page){
-	echo '<li class="page-item">
-		<a class="page-link" href="donar-list.php?page='.($num_page + 1).'">Next</a>
-	</li>';
-	}
+		if($num_page > 1){
+		echo '<li class="page-item">
+				<a class="page-link" href="donar-list.php?page='.($num_page - 1).'">Previous</a>
+		</li>';
+		}
+		for($i = 1; $i <= $total_pages; $i++){
+		if($i == $num_page){
+		$active = "active";
+		} else {
+		$active = "";
+		}
+		echo '<li class="page-item '.$active.'"><a class="page-link" href="donar-list.php?page='.$i.'">'.$i.'</a></li>';
+		}
+		if($total_pages > $num_page){
+		echo '<li class="page-item">
+				<a class="page-link" href="donar-list.php?page='.($num_page + 1).'">Next</a>
+		</li>';
+		}
 echo '</ul>';
 }
 // ===================== pagination excute end===============
